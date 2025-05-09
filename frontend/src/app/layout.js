317d9629b31abf51909ce6c2d35229d5fe93b6c8
kajtek <kajtek0123@gmail.com>
2025-05-09 22:40:39 +0200

    feat: add Bootstrap CSS

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,4 +1,10 @@
+// src/app/layout.js - global layout
 import "./globals.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export const metadata = {
+    title: 'forum',
+}
 
 export default function RootLayout({ children }) {
   return (
