6ff99cfecae1fa3e52b8c35d8e11d0402760b00f
kajtek <kajtek0123@gmail.com>
2025-05-10 00:23:39 +0200

    feat: authentication mock in progress

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,17 +1,20 @@
 // src/app/layout.js - global layout
 import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css'
+import UserProvider from "./context/UserContext"
 
 export const metadata = {
     title: 'forum',
 }
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>
+                <UserProvider>
+                    {children}
+                </UserProvider>
+            </body>
+        </html>
+    );
 }
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,5 +1,13 @@
+"use client";
+import useAuth from "./hooks/useAuth";
+import { useUser } from "./context/UserContext";
+
 export default function Home() {
-  return (
-    <h1>Hello World!</h1> 
-  );
+    useAuth();
+
+    const { loggedInUser } =  useUser();
+
+    return (
+        <h1>Hello, {loggedInUser.name}!</h1> 
+    );
 }
